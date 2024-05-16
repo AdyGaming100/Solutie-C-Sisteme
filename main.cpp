0e5458d5c6e18f72a14b7eaf6eb933cd53bb37fb
adypicolo.exe <34213400+adypicolo@users.noreply.github.com>
2024-05-17 00:27:11 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -87,9 +87,9 @@ double rang(double A[][4]){
 
     for (int i = 0; i < 3; i++)
         for (int j = 0; j < 4; j++)
-            if (A[i][j] != 0) return 1;
+            if (A[i][j] == 0) return 0;
 
-    return 0;
+    return 1;
 }
 
 int main()
